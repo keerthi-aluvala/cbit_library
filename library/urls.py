0db06ba2a5d8bae9e8b64cbc4705c9a2a740d48f
Keerthi Aluvala <keerthialuvala7@gmail.com>
2021-03-30 20:44:40 +0530

    cbit library

--- library/urls.py
+++ library/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='library-home'),
+    path('sems/', views.sems, name='library-sems'),
 ]
--- library/views.py
+++ library/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def home(request):
 	return render(request,'branches.html')
+
+def sems(request):
+	return render(request,'sems.html') 
