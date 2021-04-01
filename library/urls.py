90bfafc996c491ccec7cad14659874b3891baa76
Keerthi Aluvala <keerthialuvala7@gmail.com>
2021-04-01 17:46:09 +0530

    initial commit

--- library/urls.py
+++ library/urls.py
@@ -3,5 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='library-home'),
-    path('sems/', views.sems, name='library-sems'),
+    path('it_sems/', views.it_sems, name='library-it-sems'),
+    path('cse_sems/', views.cse_sems, name='library-cse-sems'),
+    path('ece_sems/', views.ece_sems, name='library-ece-sems'),
+    path('eee_sems/', views.eee_sems, name='library-eee-sems'), 
+    path('it_sem3/', views.it_sem3, name='library-it-sem3'), 
+
 ]
--- library/views.py
+++ library/views.py
@@ -2,7 +2,20 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-	return render(request,'branches.html')
+	return render(request,'home.html')
 
-def sems(request):
-	return render(request,'sems.html') 
+def it_sems(request):
+	return render(request,'it_sems.html') 
+
+def cse_sems(request):
+	return render(request,'cse_sems.html') 
+
+def ece_sems(request):
+	return render(request,'ece_sems.html') 
+
+def eee_sems(request):
+	return render(request,'eee_sems.html') 
+
+def it_sem3(request):
+	return render(request,'it_sem3.html') 
+
